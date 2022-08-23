f65e266a750cb083ad628395c76841f286246931
truski <98270499+truski42@users.noreply.github.com>
2022-08-23 16:57:34 +0200

    Delete RG

--- RPG_game/text-game/Introduction/langEN.cpp
+++
@@ -1,8 +0,0 @@
-   #include <iostream>
-   #include "langEN.hpp"
-   void en::introducitonEN(){
-   std::cout << "You have set out on a long journey in search of new adventures and a good income, because your village has been taken over by the enemy army and all the settlers have been exterminated.\nAfter several long and exhausting days of wandering, you came upon a settlement, where you were greeted by the local mayor.\n" << std::endl;
-    std::cout << "Hello, my name is Leo, I'm the village chief here." << std::endl;
-    std::cout << "What's your name, stranger?" << std::endl; // insert name.
-    std::cin.getline(nick, 50);
-   };
